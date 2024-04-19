76b6b163b3e697e897d0c15a517da1bcb36bf8f2
Andrii Pidhornyi <a.pidhornyi.dev@gmail.com>
2024-04-19 16:05:57 +0300

    feat(auth): implement send password reset link functional

--- src/routes/auth.php
+++ src/routes/auth.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\ForgotPasswordController;
 use App\Http\Controllers\Api\Auth\LoginController;
 use App\Http\Controllers\Api\Auth\LogoutController;
 use Illuminate\Support\Facades\Route;
@@ -7,6 +8,9 @@ use Illuminate\Support\Facades\Route;
 Route::name('auth.')->group(function () {
     Route::middleware(['guest'])->group(function () {
         Route::post('login', [LoginController::class, 'login'])->name('login');
+
+        Route::post('/forgot-password', [ForgotPasswordController::class, 'sendResetLinkEmail'])
+            ->name('password.forgot');
     });
 
     Route::middleware(['auth:sanctum'])->group(function () {
